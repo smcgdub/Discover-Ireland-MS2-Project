59dcee4b131f1834d25a3c3872b52cea67b148cb
smcgdub <stephen_xyz1@hotmail.com>
2021-03-08 14:35:35 +0000

    Have added the weather widget to the top of the cork page.

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,4 +1,7 @@
-// WEATHER WIDGET 
+// WEATHER WIDGET DUBLIN
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
+// WEATHER WIDGET CORK
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
 
 // YOUTUBE VIDEO
