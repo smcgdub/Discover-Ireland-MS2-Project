578eb9c500f8977700ac5702a1f6bb9a0b6ac5ec
smcgdub <stephen_xyz1@hotmail.com>
2021-03-08 23:31:15 +0000

    Have adjusted the weather widgets for each page so the display on them now reflects time of day as well as current weather conditions

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -4,6 +4,9 @@
 // WEATHER WIDGET CORK
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
 
+// WEATHER WIDGET GALWAY 
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
 // YOUTUBE VIDEO DUBLIN
 var tag = document.createElement('script');
 
