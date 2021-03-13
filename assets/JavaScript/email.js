39979b443c3df2fc7bd01300036aed653bc7841f
smcgdub <stephen_xyz1@hotmail.com>
2021-03-13 18:41:16 +0000

    Have created an email.js file and moved the code for the contact us form to this file

---
+++ assets/JavaScript/email.js
@@ -0,0 +1,21 @@
+// INITIALIZE SDK WITH USER ID
+(function() {
+      emailjs.init('user_Sxbabc5EJNTEdzpGRK5bs');
+})();
+
+// CONTACT US FORM JS CODE
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+// generate a five digit number for the contact_number variable
+// this.contact_number.value = Math.random() * 100000 | 0;
+// these IDs from the previous steps
+  emailjs.sendForm('contact_service', 'contact_form', this)
+    .then(function() {
+      alert('Your message has been sent successfully');
+    }, function(error) {
+      alert('Message not sent, please try again', error);
+    });
+  });
+  document.getElementById('contact-form').reset();
+}
