b76f146a18eb4a65e18dea27dc4021c338161822
smcgdub <stephen_xyz1@hotmail.com>
2021-03-16 15:53:35 +0000

    Have added the Galway video JS code and added the video to the Galway page

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -7,7 +7,8 @@ var tag = document.createElement('script');
   tag.src = "https://www.youtube.com/iframe_api";
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-// LOAD PLAYERS
+
+// LOAD VIDEO DUBLIN PLAYER
 var player;
   function onYouTubeIframeAPIReady() {
     player = new YT.Player('dublin-player', {
@@ -29,21 +30,17 @@ var player;
       'onStateChange': onPlayerStateChange
           }
         });
-
+// LOAD VIDEO GALWAY PLAYER
+    player = new YT.Player('galway-player', {
+    height: '600',
+    width: '100%',
+    videoId: 'OmiCXTSg5lk',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+          }
+        });
       }
-
-// var player;
-//   function onYouTubeIframeAPIReady() {
-//     player = new YT.Player('cork-player', {
-//     height: '450',
-//     width: '100%',
-//     videoId: 'zfMbpxt9Qgo',
-//     events: {
-//       'onReady': onPlayerReady,
-//       'onStateChange': onPlayerStateChange
-//           }
-//         });
-//       }
 
 // AUTO PLAY FUNCTION SET TO NONE
 function onPlayerReady(event) {
@@ -60,38 +57,3 @@ var done = false;
   function stopVideo() {
     player.stopVideo();
 }
-
-// YOUTUBE VIDEO CORK
-// var tag = document.createElement('script');
-
-  // tag.src = "https://www.youtube.com/iframe_api";
-  // var firstScriptTag = document.getElementsByTagName('script')[0];
-  // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// var player;
-//   function onYouTubeIframeAPIReady() {
-//     player = new YT.Player('cork-player', {
-//     height: '450',
-//     width: '100%',
-//     videoId: 'zfMbpxt9Qgo',
-//     events: {
-//       'onReady': onPlayerReady,
-//       'onStateChange': onPlayerStateChange
-//           }
-//         });
-//       }
-
-// function onPlayerReady(event) {
-//   event.target.playVideo();
-// }
-
-// var done = false;
-//   function onPlayerStateChange(event) {
-//     if (event.data == YT.PlayerState.PLAYING && !done) {
-//       setTimeout(stopVideo,);
-//       done = true;
-//     }
-//   }
-//   function stopVideo() {
-//     player.stopVideo();
-// }
