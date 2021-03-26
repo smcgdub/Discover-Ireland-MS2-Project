1165fe6bf0bd4d950c9a2e0b1439aa2febed77bf
smcgdub <stephen_xyz1@hotmail.com>
2021-03-26 17:13:18 +0000

    Save before testing JS maps

--- assets/JavaScript/maps-galway.js
+++ assets/JavaScript/maps-galway.js
@@ -4,25 +4,25 @@
 // CITY VARIABLES 
   // const dublin = { lat: 53.34664, lng: -6.271798 }
   // const cork = { lat: 51.898627, lng: -8.4705942 }
-  const galway = { lat: 53.2744122, lng: -9.0490632 }
+  const galway = { lat: 53.2744122, lng: -9.0490632 };
 
 // ICON VARIABLES 
-  const hotel = 'http://maps.google.com/mapfiles/kml/pal2/icon20.png'
-  const bars = 'http://maps.google.com/mapfiles/kml/pal2/icon19.png'
-  const restaurant = 'http://maps.google.com/mapfiles/kml/pal2/icon63.png'
-  const touristAttractions = 'http://maps.google.com/mapfiles/kml/pal4/icon46.png'
+  const hotel = 'http://maps.google.com/mapfiles/kml/pal2/icon20.png';
+  const bars = 'http://maps.google.com/mapfiles/kml/pal2/icon19.png';
+  const restaurant = 'http://maps.google.com/mapfiles/kml/pal2/icon63.png';
+  const touristAttractions = 'http://maps.google.com/mapfiles/kml/pal4/icon46.png';
 
 // MAP FUNCTION START
   function initMap() {
 
     // MAP OPTIONS 
-    const mapProp = {
+    const mapOptions = {
       center: galway,
       zoom: 15,
     };
 
 // CREATE THE MAP 
-  map = new google.maps.Map(document.getElementById("map"), mapProp);
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
 // MY MARKERS GALWAY HOTELS
 var galwayHotels = [
