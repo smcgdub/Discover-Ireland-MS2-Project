05d4f84e3a6b5920ea9fa2c5ec52f46bc8d60caf
smcgdub <stephen_xyz1@hotmail.com>
2021-04-03 15:07:24 +0000

    Have corrected the info window on cork map for Elizabeth fort, public museum and the new bar

--- test-code.js
+++
@@ -1,5 +0,0 @@
-`<b>Address</b>: Sir John Rogerson's Quay, Docklands, Dublin, D02 YT21 <br>
-                        <b>Phone</b>: +353 1 643 9500 <br>
-                        <b>About</b>: Modern hotel in a seven storey, contemporary landmark building 
-                        <br><br>
-                        <a href="https://www.claytonhotelcardifflane.com/" target=”_blank”></a>`
