d94a760d26a5bd6c28b6e82cf31d81789371077d
smcgdub <stephen_xyz1@hotmail.com>
2021-04-07 16:29:29 +0000

    Have added the stopVideo time to the app.js file line 55

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -52,7 +52,7 @@ function onPlayerReady(event) {
 var done = false;
   function onPlayerStateChange(event) {
     if (event.data == YT.PlayerState.PLAYING && !done) {
-      setTimeout(stopVideo);
+      setTimeout(stopVideo, 125000);
       done = true;
     }
   }
